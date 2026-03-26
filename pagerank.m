4d75da091f44176cc939300e483d01c33fc766b2
trebuH <hu_mu@wp.pl>
2026-03-26 13:51:12 +0100

    Add script that calculates PageRank vector based on B matrix and damping coefficient (d)

---
+++ pagerank.m
@@ -0,0 +1,36 @@
+function R = pagerank(B, d)
+    N = size(B, 1);
+
+    % When a page does not have any outgoing link it is assumed i links to
+    % every other page except itself
+    for j = 1:N
+        if sum(B(:, j)) == 0
+            B(:, j) = 1;
+            B(j, j) = 0;
+        end
+    end
+
+    L = sum(B, 1);
+    Lambda = diag(1 ./ L);
+    E = ones(N);
+    M = d * B * Lambda + ((1 - d) / N) * E;
+
+    R = ones(N, 1) / N;
+    tol = 1e-8;
+    i_max = 1000;
+
+    for i = 1:i_max
+        R_prev = R;
+
+        R = M * R;
+        R = R / norm(R, 1);
+        
+        if norm(R - R_prev) < tol
+            break;
+        end
+    end
+
+    lambda_val = (R' * M * R) / (R' * R);
+    fprintf("Convergence after %i iterations\n", i);
+    fprintf("Lambda value: %f \n", lambda_val);
+end
